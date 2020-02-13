24fa68fc89d940a6d68d3183ddfe5526632dc63a
20spencerbutler <20spencer.butler@boiseschools.net>
2020-02-13 12:33:59 -0700

    tdGame is mostly finished

--- tdGame.py
+++ tdGame.py
@@ -1,4 +1,40 @@
 import pygame
+from Map import Map
+from Projectile import Projectile
+from Tower import Tower
+
+towerProps = (
+    {
+        'range': 100,
+        'appearance': pygame.image.load('Tiles/A.png'),
+        'fireRate': 20,
+        'projectile': {
+            'damage': 10,
+            'appearance': [pygame.image.load('Tiles/B.png')],
+            'speed': 30,
+            'effects': False
+        },
+        'shop': False
+    }
+)
+
+class writerMine:
+    def __init__(self, fonter = 't'):
+        self.fonts = []
+        for i in range(0, 100):
+            self.fonts.append(i)
+            #print(i)
+        self.fontuse = fonter
+
+    def addFont(self, size):
+        self.fonts[size - 1] = pygame.font.SysFont(self.fontuse, size)
+
+    def write(self, size, text):
+        #print(str(size) + ', ' + text)
+        text = str(text)
+        if self.fonts[size - 1] == size - 1: self.addFont(size)
+        disper = self.fonts[size - 1].render(text, True, (255, 255 , 255))
+        return disper
 
 def isBounded(low, val, hi,):
     return val >= low and val <= hi
@@ -17,8 +53,20 @@ class Game():
         self.display.blit(pygame.transform.scale(self.map.getSurface(), self.menuBoundary, self.menuBoundary), (0, 0))
         self.shopNormal = shopDefault
         self.shop = self.shopNormal
+        self.towerShopOpen = -1
+        self.buyingTower = False
+        self.printer = writerMine('')
 
-    def gameTick(self):
+    def bottomMenu(self, hoverSurface = False):
+        menuSurf = pygame.Surface(1000 - self.menuBoundary, self.menuBoundary)
+        menuSurf.blit(self.printer.write(self.money, 20), (50, 50))
+        menuSurf.blit(self.printer.write(self.lives, 20), (550, 50))
+        if(hoverSurface): menuSurf.blit(pygame.scalehoverSurface, (600, 0))
+        return menuSurf
+
+
+    def gameTick(self, mousePos):
+        mx, my = mousePos[0], mousePos[1]
         dispNow = pygame.Surface(1000, 1000)
         dispNow.blit(self.display, (0, 0))
         for tower in range(0, len(self.towers)):
@@ -53,15 +101,28 @@ class Game():
             else:
                 dispNow.blit(projectile.appearance, (projectile.rect[0], projectile.rect[1]))
 
+        if self.buyingTower:
+            r = self.rangeOfBuying
+            rangeSurf = pygame.Surface((2 * r, 2 * r), pygame.SRCALPHA)
+            rangeSurf.fill((0, 0, 0, 0))
+            pygame.draw.circle(rangeSurf, (200, 0, 0, .5), (r / 2, r / 2), r)
+            dispNow.blit(rangeSurf, (mx + r, my + r))
 
+        dispNow.blit(pygame.transform.scale(self.shop.getSurface(), (1000 - self.menuBoundary, 1000)), (self.menuBoundary, 0))
+        if(mx > self.menuBoundary):
+            buttonHovered = self.shop.click((mx - self.menuBoundary, y))
+            if(buttonHovered):
+                currentHoverSurface = buttonHovered.description
+        dispNow.blit(self.bottomMenu(currentHoverSurface), (0, self.menuBoundary))
 
         return dispNow
 
     def handleClick(self, position):
         x, y = position[0], position[1]
-        for tower in self.towers:
+        for tower in range(0, len(self.towers)):
             if isBounded(tower.position[0], x, tower.position[0] + 50) and isBounded(tower.position[1], y, tower.position[1] + 50):
-                self.shop = tower.shop
+                self.shop = self.towers[tower].shop
+                self.towerShopOpen = tower
                 return
 
         if isBounded(self.menuBoundary, x, self.display.get_width()):
@@ -70,7 +131,39 @@ class Game():
                 type = buttonClicked.onClick()
                 if not type:
                     return
-                
+                if type[0] == 'sell':
+                    self.towers.pop(self.towerShopOpen)
+                    self.shop = self.shopNormal
+                    self.towerShopOpen = -1
+                    self.money += 1
+
+                if type[0] == 'upgrade':
+                    self.towers[self.towerShopOpen].upgrade()
+
+                # _range, _appearance, _fireRate, _projectile, _location , _shop
+                # (self, _posX, _posY, _damage, _appearance, _movementSpeed, _effects):
+
+                if type[0] == 'buyTower':
+                    s = towerProps[type[1]]
+                    self.buyingTower = True
+                    self.rangeOfBuying = s['range']
+                    self.towerBuying = type[1]
+
+        if self.buyingTower:
+            self.buildTower(self.towerBuying, (x, y))
+
+        self.buyingTower = False
+        self.shop = self.shopNormal
+        self.towerShopOpen = -1
+
+    def buildTower(self, tower, loc):
+        s = towerProps[tower]
+        t = s['projectile']
+        self.towers.append(
+            Tower(s['range'], s['appearance'], s['fireRate'],
+                  Projectile(loc[0], loc[1], t['damage'], t['appearance'], t['speed'], t['effects']),
+                  (loc[0], loc[1]), s['shop']))
+
 
 
 
