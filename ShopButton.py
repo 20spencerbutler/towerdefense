b4e9755a84e36abc164647f1ecc1cf427db73c57
aiaiBSD <54329973+aiaiBSD@users.noreply.github.com>
2020-02-19 12:54:34 -0700

    Add files via upload

--- ShopButton.py
+++ ShopButton.py
@@ -15,7 +15,7 @@ class ShopButton():
         self.description = _description
         self.type = _type
         self.display = pygame.Surface((800, 600))
-        self.icon = pygame.display.set_mode((30, 30))
+        self.icon = pygame.Surface((60, 60))
 
         font = pygame.font.Font('freesansbold.ttf', 20)
         pygame.draw.rect(self.display, GRAY, (50, 200, 500, 200))
@@ -36,9 +36,9 @@ class ShopButton():
             textSurfUpgrade, textRectUpgrade = makeText("Upgrades For: " + (str)(self.cost), WHITE, GRAY, 50, 379, font)
             self.display.blit(textSurfUpgrade, textRectUpgrade)
 
-        iconFont = pygame.font.Font('freesansbold.ttf', 5)
+        iconFont = pygame.font.Font('freesansbold.ttf', 10)
         self.icon.fill(GRAY)
-        pygame.draw.circle(self.icon, BLACK, (15, 15), 15)
+        pygame.draw.circle(self.icon, BLACK, (30, 30), 30)
         self.iconName = []
         start = 0
         for i in range (len(self.name)):
@@ -46,30 +46,29 @@ class ShopButton():
                 item = self.name[start:i]
                 self.iconName.append(item)
                 start = i + 1
-                item += 1
             elif i == len(self.name) - 1:
-                item = self.name[start:i]
+                item = self.name[start:i + 1]
                 self.iconName.append(item)
-        yPos = 5
+        yPos = 10
         for i in self.iconName:
-            textSurf, textRect = makeText(i, WHITE, BLACK, 5, yPos, font)
+            textSurf, textRect = makeText(i, WHITE, BLACK, 10, yPos, iconFont)
             yPos += 10
+            self.icon.blit(textSurf, textRect)
 
-        while True:
-            for event in pygame.event.get(QUIT):
-                pygame.quit()
-                sys.exit()
-            for event in pygame.event.get(KEYUP):
-                if event.key == K_ESCAPE:
-                    pygame.quit()
-                    sys.exit()
-                pygame.event.post(event)
-            pygame.display.update()
+    def onClick(self, bank):
+        if bank < self.cost:
+            return False
+        else:
+            return self.type[0]
+
+    def getDescription(self):
+        return self.display
+
+    def getIcon(self):
+        return self.icon
 
 def makeText(text, color, bgcolor, top, left, font):
     textSurf = font.render(text, True, color, bgcolor)
     textRect = textSurf.get_rect()
     textRect.topleft = (top, left)
     return (textSurf, textRect)
-
-button = ShopButton(255, "Blaster", ["This item blasts things."], ["Buy", "Tower"])
