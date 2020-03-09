95b9b00babaac1311c0202c5f445f862a01feb54
20spencerbutler <20spencer.butler@boiseschools.net>
2020-03-09 13:09:26 -0600

    You now lose! Huzzah!

--- MainLoop.py
+++ MainLoop.py
@@ -1,10 +1,9 @@
-import pygame, sys, random, time
+import pygame, sys
 from pygame.locals import *
 from ShopButton import ShopButton
 from Shop import Shop
 from tdGame import Game
 from Menu import Menu
-from Map import Map
 #pygame.init()
 
 basicTowerShop = Shop([
@@ -72,14 +71,17 @@ for i in range(0, len(towerProps)):
 
 mainShop = Shop(shopButtons)
 
+bigFont = pygame.font.SysFont('Comic Sans MS', 50)
+
 def main():
-    global WINDOWWIDTH, WINDOWHEIGHT, DISPLAYSURF, ISGAME
+    global WINDOWWIDTH, WINDOWHEIGHT, DISPLAYSURF, ISGAME, bigFont
     WINDOWWIDTH = 1000
     WINDOWHEIGHT = 1000
     FPS = 40
     FPSCLOCK = pygame.time.Clock()
     DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), RESIZABLE)
     ISGAME = False
+    hasLost = False
     mousepos = None
     runningGame = None
     gameMenu = Menu()
@@ -106,7 +108,16 @@ def main():
             dispLastTick = runningGame.gameTick(mousepos)
             if mousepos[0] >= 0 and mousepos[0] <= 1000 and mousepos[1] >= 0 and mousepos[1] <= 1000 and isClick:
                 runningGame.handleClick(mousepos)
-            drawSurface(dispLastTick)
+            if dispLastTick == 'lost!':
+                drawSurface(bigFont.render('You lose!!', True, (255, 255, 255)))
+                ISGAME = False
+                hasLost = True
+            else:
+                drawSurface(dispLastTick)
+        elif(hasLost):
+            drawSurface(bigFont.render('You lose!!', True, (255, 255, 255)))
+            if isClick:
+                hasLost = False
         else:
             mousepos = gameClick(mousepos)
             if mousepos[0] >= 0 and mousepos[0] <= 1000 and mousepos[1] >= 0 and mousepos[1] <= 1000 and isClick:
